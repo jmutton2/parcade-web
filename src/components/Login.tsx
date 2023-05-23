caaa14783a5d5bd8591a007846783165e06a21f9
Jacob Mutton <jmutton2@uwo.ca>
2023-05-23 12:33:14 -0400

    Copied styling to signup

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -51,7 +51,6 @@ const Login = () => {
 											aria-describedby="remember"
 											type="checkbox"
 											className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-											required={true}
 										></input>
 									</div>
 									<div className="ml-3 text-sm">
